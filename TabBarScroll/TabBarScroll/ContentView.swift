932dce71e8dc2db149473f83ca4fe4f16b7e0923
Mohit Gupta <Mohit.Gupta14@ibm.com>
2026-04-23 13:45:44 +0530

    Added TabbarScroll for ContentView

--- TabBarScroll/TabBarScroll/ContentView.swift
+++ TabBarScroll/TabBarScroll/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isScrolledUp : Bool = false
+    @State private var storedOffset : CGFloat = 0
+    @State private var scrollPhase : ScrollPhase = .idle
+    @State private var tabBarVisibility : Visibility = .visible
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            Tab.init("For You", systemImage : "heart.text.square.fill"){
+                ScrollView(.vertical){
+                    VStack(spacing : 12){
+                        ForEach(1...50, id : \.self){ _ in
+                            Rectangle()
+                                .fill(.fill.tertiary)
+                                .frame(height : 50)
+                        }
+                    }.padding(15)
+                }.onScrollGeometryChange(for: CGFloat.self) {
+                    $0.contentOffset.y + $0.contentInsets.top
+                } action: { oldValue, newValue in
+                    let isScrolledUp = oldValue < newValue
+                    if  self.isScrolledUp != isScrolledUp{
+                        // Storing content off set
+                        storedOffset = newValue - (tabBarVisibility == .hidden ? 60 : 0)
+                        self.isScrolledUp = isScrolledUp
+                    }
+                    let diff = newValue - storedOffset
+                    if scrollPhase == .interacting{
+                        tabBarVisibility = diff > 50 ? .hidden : .visible
+                    }
+                }
+                .onScrollPhaseChange { oldPhase, newPhase in
+                    scrollPhase = newPhase
+                }
+                .toolbarVisibility(tabBarVisibility, for: .tabBar)
+                .animation(.smooth(duration: 0.3, extraBounce: 0), value: tabBarVisibility)
+            }
+            Tab.init("Products", systemImage : "macbook.and.iphone"){
+                
+            }
+            Tab.init("More", systemImage : "safari"){
+                
+            }
+            Tab.init("Bag", systemImage : "bag"){
+                
+            }
+            Tab.init(role : .search){
+                
+            }
         }
-        .padding()
+        .tabViewBottomAccessory {
+            Text("Welcome")
+                .font(.caption)
+                .multilineTextAlignment(.center)
+        }.tabBarMinimizeBehavior(.onScrollDown)
     }
 }
 
