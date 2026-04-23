932a7cbea29af688b029d7edbbd536e61b9d687d
Mohit Gupta <Mohit.Gupta14@ibm.com>
2026-04-23 13:16:06 +0530

    First Commit

---
+++ TabBarScroll/TabBarScroll/TabBarScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarScrollApp.swift
+//  TabBarScroll
+//
+//  Created by Mohit  on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TabBarScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
